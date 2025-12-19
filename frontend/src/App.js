9b862f89178075e3fa30d7e38c31600a1d5cb517
callmhegeorge-hash <callmhegeorge@gmail.com>
2025-12-19 10:48:04 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,15 @@
+import React from "react";
+import DonationForm from "./components/DonationForm";
+import DonationList from "./components/DonationList";
 
+function App() {
+  return (
+    <div style={{ maxWidth: "600px", margin: "0 auto", padding: "2rem" }}>
+      <h1>Orphanage Donation</h1>
+      <DonationForm />
+      <DonationList />
+    </div>
+  );
+}
+
+export default App;
