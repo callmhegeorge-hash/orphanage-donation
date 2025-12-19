c49b3874aabadc62d20042bcc75c346e5c47747d
callmhegeorge-hash <callmhegeorge@gmail.com>
2025-12-19 10:49:16 +0000

    Update DonationList.js

--- frontend/src/components/DonationList.js
+++ frontend/src/components/DonationList.js
@@ -1,1 +1,25 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
+const DonationList = () => {
+    const [donations, setDonations] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/api/donations")
+            .then(res => setDonations(res.data))
+            .catch(err => console.log(err));
+    }, []);
+
+    return (
+        <div>
+            <h2>Recent Donations</h2>
+            <ul>
+                {donations.map(d => (
+                    <li key={d._id}>{d.name} donated ${d.amount}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default DonationList;
